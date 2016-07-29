0ee6c365f1091cac870713ab7dcd49ce73a1c7c6
Gavin Guinane <gguinane@gguinane-ltm.internal.salesforce.com>
2016-07-29 13:24:40 +0100

    ok

--- MCPushApp/ViewController.h
+++ MCPushApp/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *Connect;
+@property (weak, nonatomic) IBOutlet UITextField *SubscriberId;
+@property (weak, nonatomic) IBOutlet UILabel *HelloLabel;
 
 @end
 
--- MCPushApp/ViewController.m
+++ MCPushApp/ViewController.m
@@ -16,12 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //// Do any additional setup after loading the view, typically from a nib.
+    //UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+    //label.text = @"Hello again!";
+    //[self.view addSubview:label];
+    
+    self.HelloLabel.text = @"Subscriber ID";
+    [self.Connect setTitle:@"Send to Marketing Cloud" forState:UIControlStateNormal];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)handleButtonClick:(id)sender {
+    self.SubscriberId.text = @"HolyMoly";
 }
 
 @end
